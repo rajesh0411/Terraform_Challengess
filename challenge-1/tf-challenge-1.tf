e759325d7ae7c685ce36ce1cf294b106768ffbe8
rajesh0411 <rajeshc0411@gmail.com>
2023-10-09 14:43:52 +0530

    Update tf-challenge-1.tf

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.54"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 2.54"
   region  = "us-east-1"
 
 }
