de25f8aed618ef03d3f0e54871e4888fa03b1ac8
rajesh0411 <rajeshc0411@gmail.com>
2023-10-09 14:39:47 +0530

    Update tf-challenge-1.tf

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,8 +1,7 @@
 provider "aws" {
   version = "~> 2.54"
   region  = "us-east-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
 }
 
 provider "digitalocean" {}
